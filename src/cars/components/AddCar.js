325bb97fb68c709518ab6279c491e3208eb60ed3
alexb997 <alex_alexandru_997@yahoo.ro>
2021-11-29 13:26:35 +0200

    Post request

--- src/cars/components/AddCar.js
+++ src/cars/components/AddCar.js
@@ -1,15 +1,84 @@
-import React from 'react';
-import CarForm from '../forms/CarForm';
+import { useEffect, useState } from "react";
+import CarContainer from "./CarContainer";
 
 const AddCar = () => {
-  const handleOnSubmit = (car) => {
-    console.log(car);
+  const [car, setCar] = useState({});
+  const [plate, setPlate] = useState("");
+  const [manufacturer, setManufacturer] = useState("");
+
+  useEffect(() => {
+    setCar({
+      plate: plate,
+      manufacturer: manufacturer,
+      assured: false,
+    });
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        plate: plate,
+        manufacturer: manufacturer,
+      }),
+    };
+    fetch("http://localhost:8080/api/cars/", requestOptions)
+      .then((response) => response.json())
+      .then((data) => setCar(data));
+  };
+  const manufacturerChange = (e) => {
+    setManufacturer(e);
+    setCar({
+      plate: plate,
+      manufacturer: manufacturer,
+      assured: false,
+    });
+  };
+  const plateChange = (e) => {
+    setPlate(e);
+    setCar({
+      plate: plate,
+      manufacturer: manufacturer,
+      assured: false,
+    });
   };
 
   return (
-    <React.Fragment>
-      <CarForm handleOnSubmit={handleOnSubmit} />
-    </React.Fragment>
+    <div>
+      <h3>Input form</h3>
+      <form
+        onSubmit={(e) => {
+          handleSubmit(e);
+        }}
+      >
+        <label>
+          Plate:
+          <input
+            type="text"
+            name="plate"
+            placeholder={car.plate}
+            onChange={(e) => plateChange(e.target.value)}
+          />
+        </label>
+        <label>
+          Manufacturer:
+          <input
+            type="text"
+            name="manufacturer"
+            placeholder={car.manufacturer}
+            onChange={(e) => manufacturerChange(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      <hr />
+      <h3>Car to be added</h3>
+      <CarContainer car={car} />
+    </div>
   );
 };
 
--- src/cars/routes/Router.js
+++ src/cars/routes/Router.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router ,Routes , Route } from 'react-router-dom';
-import AddCar from '../components/AddCar';
-import CarsList from '../components/CarsList';
-import CarById from '../components/CarById';
-import CarsByManufacturer from '../components/CarsByManufacturer';
-import UpdateCar from '../components/UpdateCar';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AddCar from "../components/AddCar";
+import CarsList from "../components/CarsList";
+import CarById from "../components/CarById";
+import CarsByManufacturer from "../components/CarsByManufacturer";
+import UpdateCar from "../components/UpdateCar";
 
 const AppRouter = () => {
   return (
     <Router>
-        <Routes>
-            <Route path="/cars" element={<CarsList/>}  />
-            <Route path="/cars/:id" element={<CarById/>}/>
-            <Route path="/carsBy/:manufacturer" element={<CarsByManufacturer/>}/>
-            <Route path="/updateCar/:id" element={<UpdateCar/>}/>
-            <Route path="/add" element={<AddCar/>}  />
-        </Routes>
+      <Routes>
+        <Route path="/cars" element={<CarsList />} />
+        <Route path="/cars/:id" element={<CarById />} />
+        <Route path="/carsBy/:manufacturer" element={<CarsByManufacturer />} />
+        <Route path="/updateCar/:id" element={<UpdateCar />} />
+        <Route path="/add" element={<AddCar />} />
+      </Routes>
     </Router>
   );
 };
