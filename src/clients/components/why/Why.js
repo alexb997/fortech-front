8401018208f58f1d57be48e8c030bd21efb6ff82
alexb997 <alex_alexandru_997@yahoo.ro>
2022-02-09 20:38:07 +0200

    route

---
+++ src/clients/components/why/Why.js
@@ -0,0 +1,295 @@
+import { Card, Col, Container, Row, Button } from "react-bootstrap";
+import NavBar from "../../../navigation/navBar";
+
+function Why() {
+  return (
+    <div>
+      <NavBar />
+      <div className="empty"></div>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text center">
+              <b>Why CarTalk</b>
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+      <div className="empty"></div>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text">
+              <b>We only sell the best</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" sm={4}>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text center">
+              <Button variant="outline-warning">Button to shop</Button>
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text">
+              <b>Stress-free car buying</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" sm={4}>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text center">
+              <Button variant="outline-warning">Button to help-center</Button>
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text">
+              <b>We've got your back</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" sm={4}>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text center">
+              <Button variant="outline-warning">Find your car</Button>
+            </h2>
+          </Col>
+        </Row>
+      </Container>
+      <Container>
+        <Row className="justify-content-center">
+          <Col>
+            <h2 className="blue-text">
+              <b>The most flexible car-buying experience ever</b>
+            </h2>
+          </Col>
+        </Row>
+        <Row className="justify-content-center" sm={4}>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+export default Why;
--- src/clients/routes/Router.js
+++ src/clients/routes/Router.js
@@ -7,11 +7,14 @@ import UpdateClient from "../components/clients/UpdateClient";
 import AssuranceCreator from "../../admins/components/assurances/AssuranceCreator";
 import AssuranceList from "../../admins/components/assurances/AssuranceList";
 import Home from "../components/home/Home.js";
+import Finances from "../components/finances/Finances.js";
 import AddCar from "../../admins/components/cars/AddCar";
 import CarById from "../components/cars/CarById";
 import CarsList from "../components/cars/CarsList";
 import Profile from "../components/profile/Profile";
 import Admin from "../../admins/admin";
+import Services from "../components/services/Services";
+import Why from "../components/why/Why";
 
 function AppRouter() {
   return (
@@ -24,9 +27,12 @@ function AppRouter() {
         <Route path="/car/:id" element={<CarById />} />
         <Route path="/cars" element={<CarsList />} />
         <Route path="/admin" element={<Admin />} />
+        <Route path="/why-CarTalk" element={<Why />} />
         {/* <Route path="/assurance/:id" element={<AssurancePicker />} /> */}
         {/* <Route path="/update-assurance-plan/:id" element={<AssuranceUpdater />} /> */}
         <Route path="/add-insurance-plan" element={<AssuranceCreator />} />
+        <Route path="/finances" element={<Finances />} />
+        <Route path="/services" element={<Services />} />
         {/* <Route path="/remove-assurance-plan/:id" element={<AssuranceRemover />} /> */}
         <Route path="/clients" element={<ClientsList />} />
         <Route path="/clients/add" element={<AddClient />} />
